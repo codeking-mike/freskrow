7c661c07f38e672c770967ed62f7fd2025a1d1a7
Michael Idam <princemicare@gmail.com>
2026-06-10 14:38:54 +0100

    updated project configuration

--- frontend/lib/wagmi.ts
+++ frontend/lib/wagmi.ts
@@ -14,9 +14,11 @@ import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { sepolia } from 'wagmi/chains';
 import { http } from 'viem';
 
+
 export const config = getDefaultConfig({
   appName: 'FresCrow Platforms',
-  projectId: '6d767ec18b127a615b3c4e85397e823e', 
+  // Look for the Vercel environment variable first, fall back to your ID locally if needed
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '6d767ec18b127a615b3c4e85397e823e', 
   chains: [sepolia],
   transports: {
     [sepolia.id]: http(),
